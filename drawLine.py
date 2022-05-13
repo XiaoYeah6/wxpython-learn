5f1c7d7a8fc4a46d00d84b503c9c6ed76e8b8a87
yyl666818gi <yezizhuce@163.com>
2022-05-13 13:38:35 +0800

    画图

--- drawLine.py
+++ drawLine.py
@@ -1,7 +1,6 @@
 import wx
 
 
- 
 class LineChart(wx.Panel): 
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
@@ -13,12 +12,50 @@ class LineChart(wx.Panel):
         dc = wx.PaintDC(self)
         dc.SetDeviceOrigin(40, 240)
         dc.SetAxisOrientation(True, True)
-        dc.SetPen(wx.Pen('WHITE'))
+        dc.SetPen(wx.Pen('BLACK'))
+        self.DrawAxis(dc)
+        self.DrawShip(dc)
+
+    # 绘制坐标轴
+    def DrawAxis(self, dc):
+        dc.SetPen(wx.Pen('#0AB1FF'))
+        font = dc.GetFont()
+        font.SetPointSize(8)
+        dc.SetFont(font)
+        dc.DrawLine(-10, 0, 300, 0)
+        dc.DrawLine(0, -200, 0, 200)
+
+        # 绘制纵坐标间隔
+        for i in range(-200, 201, 10):
+            dc.DrawText(str(i), -30, i+5)
+            dc.DrawLine(2, i, -3, i)
+ 
+        for i in range(10, 300, 10):
+            dc.DrawLine(i, 2, i, -3)
+ 
+    def DrawShip(self, dc):
+        dc.SetPen(wx.Pen('#BLACK'))
+        font = dc.GetFont()
+        font.SetPointSize(8)
+        dc.SetFont(font)
+        # 船体
+        dc.DrawLine(100, 20, 100, -20)
+        dc.DrawLine(100, 20, 200, 20)
+        dc.DrawLine(100, -20, 200, -20)
+        dc.DrawLine(200, 20, 250, 0)
+        dc.DrawLine(200, -20, 250, 0)
+
+        dc.SetPen(wx.Pen('#BLACK', width=1, style=wx.LONG_DASH))
+        # 船尾部
+        dc.DrawLine(100, 20, 0, 60)
+        dc.DrawLine(100, -20, 0, -60)
+        dc.DrawSpline()
+
  
  
 class LineChartExample(wx.Frame):
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(450, 400))
+        wx.Frame.__init__(self, parent, id, title, size=(450, 600))
  
         panel = wx.Panel(self, -1)
         panel.SetBackgroundColour('RED')
